507ca281fcb657678cfd0f19822eb9536b282e54
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-11-06 16:56:12 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,20 @@ input.onButtonPressed(Button.A, function () {
     status = 0
 })
 input.onButtonPressed(Button.B, function () {
+    status = 1
+    if (input.lightLevel() < 10) {
+        led.setBrightness(10)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+    } else {
+        led.setBrightness(input.lightLevel())
+    }
     status = 0
 })
 let status = 0
