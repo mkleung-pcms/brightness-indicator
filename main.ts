37ddb0692c3ee261980d7b226bca650f70ae93b8
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-11-06 17:05:05 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    status = 1
+    status = 3 - status
     basic.showNumber(input.lightLevel())
-    status = 0
+    status = 3 - status
 })
 input.onButtonPressed(Button.B, function () {
-    status = 1
-    if (input.lightLevel() < 10) {
-        led.setBrightness(10)
-    } else {
-        led.setBrightness(input.lightLevel())
-    }
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    basic.pause(1000)
-    status = 0
+    status = 1 - status
 })
 let status = 0
 status = 0
 basic.forever(function () {
+    led.setBrightness(32 + input.lightLevel() * 0.875)
     if (status == 0) {
-        led.setBrightness(32 + input.lightLevel() * 0.875)
         basic.clearScreen()
         for (let index = 0; index <= Math.round(input.lightLevel() * 0.09375 + 1); index++) {
             led.plot(Math.trunc((index - 1) / 5), (index - 1) % 5)
         }
+    } else if (status == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     }
 })
